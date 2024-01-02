c93138ad038927c1bd61142e869079c50bbb46c4
Dorian Edwards <de.pharmd2018@gmail.com>
2024-01-02 10:58:11 -0500

    reorganize assets folder

---
+++ src/assets/icons/NavLogo.tsx
@@ -0,0 +1,9 @@
+import image from '../images/logo-yellow-horizontal.png'
+
+export default function NavLogo() {
+  return (
+    <div className='logo-wrapper max-w-[20.2rem]'>
+      <img src={image} alt='' />
+    </div>
+  )
+}
