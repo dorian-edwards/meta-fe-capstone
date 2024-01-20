b03003bd76383d7368b12b090712067e4b5f00dc
buddafucofibas <de.pharmd2018@gmail.com>
2024-01-20 12:43:34 -0500

    add routes and router

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,27 +1,30 @@
-import ButtonPrimary from './ButtonPrimary'
+import ButtonPrimary from "./ButtonPrimary"
+import { Link } from "react-router-dom"
 
 export default function Hero() {
   const description =
-    'We are a family owned mediterranean restaurant, focused on traditional recipes served with a modern twist.'
+    "We are a family owned mediterranean restaurant, focused on traditional recipes served with a modern twist."
   return (
-    <section id='hero' className='py-[3.5rem] bg-primary-green'>
-      <div className='content-container flex justify-between'>
-        <div className='hero-text-wrapper min-[560px]:basis-1/2 w-full shrink-0'>
-          <h1 className='text-title text-primary-yellow mb-[0.75rem]'>
+    <section id="hero" className="py-[3.5rem] bg-primary-green">
+      <div className="content-container flex justify-between">
+        <div className="hero-text-wrapper min-[560px]:basis-1/2 w-full shrink-0">
+          <h1 className="text-title text-primary-yellow mb-[0.75rem]">
             Little Lemon
           </h1>
-          <h3 className='text-subtitle text-white mb-[2rem]'>Baltimore</h3>
-          <p className='text-lead text-white min-[560px]:max-w-[25rem] w-full max-w-[75%] mb-[2rem]'>
+          <h3 className="text-subtitle text-white mb-[2rem]">Baltimore</h3>
+          <p className="text-lead text-white min-[560px]:max-w-[25rem] w-full max-w-[75%] mb-[2rem]">
             {description}
           </p>
-          <ButtonPrimary>Reserve a table</ButtonPrimary>
+          <Link to="/booking">
+            <ButtonPrimary>Reserve a table</ButtonPrimary>
+          </Link>
         </div>
-        <div className='hero-image-wrapper w-full relative min-[940px]:right-[-4rem] max-[559px]:hidden'>
-          <div className='img-wrapper absolute max-h-[450px] overflow-hidden rounded-3xl'>
+        <div className="hero-image-wrapper w-full relative min-[940px]:right-[-4rem] max-[559px]:hidden">
+          <div className="img-wrapper absolute max-h-[450px] overflow-hidden rounded-3xl">
             <img
-              className=''
-              src='/images/restauranfood.jpg'
-              alt='tray of food being held by a person in an apron'
+              className=""
+              src="/images/restauranfood.jpg"
+              alt="tray of food being held by a person in an apron"
             />
           </div>
         </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,14 @@
-import NavLogo from '../assets/icons/NavLogo'
-import NavItems from './NavItems'
+import { Link } from "react-router-dom"
+import NavLogo from "../assets/icons/NavLogo"
+import NavItems from "./NavItems"
 
 export default function Nav() {
   return (
-    <nav className='flex justify-between items-center content-container py-[2rem]'>
-      <NavLogo />
-      <ul className='navigation-menu flex gap-x-[2.6rem]'>
+    <nav className="flex justify-between items-center content-container py-[2rem]">
+      <Link to="/">
+        <NavLogo />
+      </Link>
+      <ul className="navigation-menu flex gap-x-[2.6rem]">
         <NavItems />
       </ul>
     </nav>
