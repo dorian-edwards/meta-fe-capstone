9a451d3187a05c155dc6199cd7c6d8be7ac28021
buddafucofibas <de.pharmd2018@gmail.com>
2024-01-24 07:12:44 -0500

    add mui date-picker; add styling to booking page

--- src/components/ButtonPrimary.tsx
+++ src/components/ButtonPrimary.tsx
@@ -1,8 +1,20 @@
-import { ReactNode } from 'react'
+import { ReactNode } from "react"
 
-export default function ButtonPrimary({ children }: { children: ReactNode }) {
+export default function ButtonPrimary({
+  children,
+  type,
+  sx,
+}: {
+  children: ReactNode
+  type?: "submit" | "button"
+  sx?: React.CSSProperties
+}) {
   return (
-    <button className='text-lead bg-primary-yellow rounded-3xl py-[1rem] px-[2rem]'>
+    <button
+      className="text-lead bg-primary-yellow rounded-3xl py-[1rem] px-[2rem]"
+      type={type || "button"}
+      style={sx}
+    >
       {children}
     </button>
   )
