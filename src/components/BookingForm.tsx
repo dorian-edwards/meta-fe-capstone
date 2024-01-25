1bc3b50cc3739a0c8caa992e18cd39c048e297e3
Dorian Edwards <de.pharmd2018@gmail.com>
2024-01-25 07:17:40 -0500

    add occasion selector, change dateTimePicker parameters, refactor Hero component and Booking Page

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,104 +1,48 @@
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker/DateTimePicker'
 import dayjs, { Dayjs } from 'dayjs'
-import { Form, Formik } from 'formik'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import ButtonPrimary from './ButtonPrimary'
-import Slider from '@mui/material/Slider/Slider'
+import ReservationDateTimePicker from './ReservationDateTimePicker'
+import PartySizeSelector from './PartySizeSelector'
+import OccasionSelector from './OccasionSelector'
+import React from 'react'
+import { DateTimeValidationError } from '@mui/x-date-pickers/models/validation'
 
 export default function BookingForm() {
-  const [value, setValue] = useState<Dayjs | null>(dayjs())
+  const [dateTime, setDateTime] = useState<Dayjs | null>(dayjs())
   const [guests, setGuests] = useState<number>(1)
-
-  const fivePM = dayjs().set('hour', 17).startOf('hour')
-
-  const closed = (date: Dayjs) => {
-    const day = date.day()
-
-    return day === 0 || day == 2 || day === 6
-  }
-
-  const marks = [
-    { value: 1, label: '1' },
-    { value: 2, label: '2' },
-    { value: 3, label: '3' },
-    { value: 4, label: '4' },
-    { value: 5, label: '5' },
-    { value: 6, label: '6' },
-    { value: 7, label: '7' },
-    { value: 8, label: '8' },
-    { value: 9, label: '9' },
-    { value: 10, label: '10' },
-  ]
-
-  // Set to next available day if restaurant is closed today, otherwise rounds up to nearest 30th minute
-  useEffect(() => {
-    let firstAvailableDay = dayjs()
-
-    while (closed(firstAvailableDay)) firstAvailableDay = dayjs().add(1, 'day')
-
-    if (firstAvailableDay.date() !== dayjs().date())
-      return setValue(firstAvailableDay.set('hour', 17).set('minute', 0))
-
-    if (firstAvailableDay.hour() < 17)
-      return setValue(firstAvailableDay.set('hour', 17).set('minute', 0))
-
-    setValue(
-      firstAvailableDay.minute() < 30
-        ? firstAvailableDay.set('minute', 30)
-        : firstAvailableDay.add(1, 'hour').set('minute', 0)
-    )
-  }, [])
+  const [occasion, setOccasion] = useState<string>('')
+  const [error, setError] = React.useState<DateTimeValidationError | null>(null)
 
   return (
-    <Formik initialValues={{}} onSubmit={() => console.log(value, guests)}>
-      <Form>
-        <DateTimePicker
-          label='Next available reservation'
-          views={['month', 'day', 'hours', 'minutes']}
-          name='date-time'
-          maxDate={dayjs().add(60, 'day')}
-          defaultValue={fivePM}
-          minTime={fivePM}
-          closeOnSelect={false}
-          shouldDisableDate={closed}
-          disablePast
-          value={value}
-          onError={(error) => console.log(error)}
-          sx={{
-            width: '100%',
-            maxWidth: 400,
-            marginBottom: '2rem',
-            '& input': {
-              fontSize: 16,
-            },
-            '& svg': {
-              transform: 'scale(1.25)',
-            },
+    <section className='form-wrapper pt-[6.2rem] pb-[9.2rem] min-[560px]:pt-[16rem]'>
+      <div className='content-container'>
+        <h2 className='text-title pb-[5rem]'>Reserve a table</h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            if (error) return
+
+            console.log({
+              dateTime: dateTime?.format('dddd, MMMM D [at] h:mm'),
+              guests,
+              occasion,
+            })
           }}
-        />
-        <h3 className='text-lead'>Party Size</h3>
-        <Slider
-          name='guests'
-          value={guests}
-          onChange={(e, val) => setGuests(val as number)}
-          aria-label='Temperature'
-          getAriaValueText={() => `Party of ${guests}`}
-          valueLabelDisplay='auto'
-          step={1}
-          marks={marks}
-          min={1}
-          max={10}
-          sx={{
-            display: 'block',
-            width: '100%',
-            maxWidth: 400,
-            color: '#495E57',
-          }}
-        />
-        <ButtonPrimary type='submit' sx={{ display: 'block' }}>
-          Make Your Reservation
-        </ButtonPrimary>
-      </Form>
-    </Formik>
+        >
+          <ReservationDateTimePicker
+            dateTime={dateTime}
+            setDateTime={setDateTime}
+            error={error}
+            setError={setError}
+          />
+          <h3 className='text-lead'>Party Size</h3>
+          <PartySizeSelector guests={guests} setGuests={setGuests} />
+          <OccasionSelector occasion={occasion} setOccasion={setOccasion} />
+          <ButtonPrimary type='submit' sx={{ display: 'block' }}>
+            Make Your Reservation
+          </ButtonPrimary>
+        </form>
+      </div>
+    </section>
   )
 }
--- src/routes/BookingPage.tsx
+++ src/routes/BookingPage.tsx
@@ -1,46 +1,19 @@
-import { Link } from "react-router-dom"
-import BookingForm from "../components/BookingForm"
-import ButtonPrimary from "../components/ButtonPrimary"
-import Main from "../components/Main"
+import BookingForm from '../components/BookingForm'
+import Hero from '../components/Hero'
 
 export default function Booking() {
-  const description =
-    "We are a family owned mediterranean restaurant, focused on traditional recipes served with a modern twist."
+  const imgAlt =
+    'We are a family owned mediterranean restaurant, focused on traditional recipes served with a modern twist.'
   return (
-    <Main>
-      <section id="hero" className="py-[3.5rem] bg-primary-green">
-        <div className="content-container">
-          <div className="flex justify-between">
-            <div className="hero-text-wrapper min-[560px]:basis-1/2 w-full shrink-0">
-              <h1 className="text-title text-primary-yellow mb-[0.75rem]">
-                Little Lemon
-              </h1>
-              <h3 className="text-subtitle text-white mb-[2rem]">Baltimore</h3>
-              <p className="text-lead text-white min-[560px]:max-w-[25rem] w-full max-w-[75%] mb-[2rem]">
-                {description}
-              </p>
-              <Link to="/events">
-                <ButtonPrimary>Special Events</ButtonPrimary>
-              </Link>
-            </div>
-            <div className="hero-image-wrapper w-full relative min-[940px]:right-[-4rem] max-[559px]:hidden">
-              <div className="img-wrapper absolute max-h-[450px] overflow-hidden rounded-3xl">
-                <img
-                  className=""
-                  src="/images/marissa-grootes.jpg"
-                  alt="tray of food being held by a person in an apron"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="form-wrapper pt-[6.2rem] pb-[9.2rem] min-[560px]:pt-[16rem]">
-        <div className="content-container">
-          <h2 className="text-title pb-[5rem]">Reserve a table</h2>
-          <BookingForm />
-        </div>
-      </section>
-    </Main>
+    <>
+      <Hero
+        id='hero-booking'
+        link='/events'
+        buttonText='Special Events'
+        imgUrl='/images/marissa-grootes.jpg'
+        imgAlt={imgAlt}
+      />
+      <BookingForm />
+    </>
   )
 }
